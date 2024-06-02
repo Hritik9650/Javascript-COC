75e5450c3242aee699a90a7f16c026e940ee4548
Hritik9650 <143494095+Hritik9650@users.noreply.github.com>
2024-06-02 12:31:32 +0000

    var let const

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "hritikarya9650@gmail.com"
+var accountPass = 35678275
+accountCity = "Jaipur"
+
+accountEmail = "hc@hc.com"
+accountPass = 457238
+accountCity = "Delhi"
+
+//accountId = 2 //not allowed
+console.log(accountId);

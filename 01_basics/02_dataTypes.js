196728fe2e5dcca6d36c25b74556fc4093c7b76e
Hritik9650 <143494095+Hritik9650@users.noreply.github.com>
2024-06-03 10:56:00 +0000

    Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as a newer version
+// alert(3+3) // we are using node.js, not browser
+
+console.log(3+3);
+
+console.log("Hritik")
+
+
+let  name = "Hritik"
+let age = 28
+let isLoggedIn = false
+
+//primitive datatypes
+//number => 2 to the power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefine => 
+//symbol  => unique
+
+//object
+
+console.log(typeof  null)// object
+console.log(typeof  undefined)// undefined

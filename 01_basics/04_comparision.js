25f5118a146a4d2a5ce20c036ab8323c1f3e0193
Hritik9650 <143494095+Hritik9650@users.noreply.github.com>
2024-06-04 15:38:26 +0000

    CONVERSION

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2==1);
+//console.log(2!=1);
+
+// BELOW TYPE OF CONVERSION IS NOT GOOD A WAY
+
+//console.log("2">1) 
+//console.log("02">1);
+
+//console.log(null > 0);
+//console.log(null == 0);
+//console.log(null>=0);
+
+
+//console.log(undefined == 0);
+//console.log(undefined > 0);
+//console.log(undefined < 0);
+
+// ===
+
+//console.log("2" === 2);
